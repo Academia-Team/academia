edcf986059c3eb5a050e2b968f5bdcecb10d90d9
Richard Johnston <techpowerawaits@outlook.com>
2023-07-24 16:30:56 -0600

    Make sure Su() doesn't unnecessarily corrupt registers
    The registers that are corrupted by GEMDOS calls, with the exclusion of
    d0 (which is used to return values), are restored before Su() returns.

--- src/su_asm.s
+++ src/su_asm.s
@@ -4,8 +4,31 @@
 
 			xdef			_Su
 
-_Su:		move.l			4(sp),-(sp)
+; UINT32 Su(UINT32 ssp);
+;
+; Brief: Executes the Super gemdos call.
+;
+; Details: When 0 is passed as a stack address, the mode we are in is toggled
+; with the current stack used in the changed mode. If anything else other than
+; 1 is passed, the stack address is changed to the specified address. Else, if
+; 1 is specified as parameter, it returns -1 if we are in Super mode or 0 if in
+; User mode.
+;
+; Register Table:
+; ---------------
+; d0	-	Holds the old system stack pointer so it can be returned.
+; a6	-	Holds the address of the start of the stack frame.
+
+SU_SSP:		equ				8
+
+_Su:		link			a6,#0
+			movem.l			d1-d2/a0-a2,-(sp)
+
+			move.l			SU_SSP(a6),-(sp)
 			move.w			#$20,-(sp)
 			trap			#1
 			addq.l			#6,sp
+
+			movem.l			(sp)+,d1-d2/a0-a2
+			unlk			a6
 			rts
