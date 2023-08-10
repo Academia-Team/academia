b7d2d6941480577bb8726e8a88dd1c47d8a6abd2
Richard Johnston <techpowerawaits@outlook.com>
2023-08-09 18:59:43 -0600

    Add getToggleName() to toggle.h
    For consistency with bool.h.

--- src/toggle.h
+++ src/toggle.h
@@ -15,4 +15,25 @@ typedef enum
 	ON  = 1
 } Toggle;
 
+/**
+ * @brief The maximum number of characters returned by getToggleName().
+ */
+#define MAX_TOGGLE_NAME_LEN 7
+
+/**
+ * @brief A type of string guaranteed to be able to hold the string returned by
+ * getToggleName().
+ */
+typedef char ToggleName[MAX_TOGGLE_NAME_LEN + 1];
+
+/**
+ * @brief Returns the friendly name of a toggle value (ON/OFF).
+ * 
+ * @param toggleVal The toggle value to read.
+ * @return The name of the toggle value given or "UNKNOWN" if the given value
+ * is not recognized.
+ */
+#define getToggleName(toggleVal) \
+	(toggleVal == OFF ? "OFF" : (toggleVal == ON ? "ON" : "UNKNOWN"))
+
 #endif
