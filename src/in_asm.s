55ece1bc1e141196082b18cb058f8632807429b4
Richard Johnston <techpowerawaits@outlook.com>
2023-08-11 11:52:23 -0600

    Move mouse management in keyboard ISR to separate subroutine

--- src/in_asm.s
+++ src/in_asm.s
@@ -24,8 +24,8 @@ IKBD_MAX_SCANCODE:		equ		$72
 IKBD_MIN_SCANCODE:		equ		$01
 
 IKBD_MIN_MOUSE_PKT_VAL:	equ		$F8
-IKBD_RCLICK_BIT:		equ		0
-IKBD_LCLICK_BIT:		equ		1
+MOUSE_RCLICK_BIT:		equ		0
+MOUSE_LCLICK_BIT:		equ		1
 
 IKBD_CAPS_BITMASK:		equ		$10
 IKBD_CTRL_BITMASK:		equ		$04
@@ -50,7 +50,6 @@ IKBD_MFP_SERVICE_BIT:	equ		6
 ; a1	-	Holds the MIDI status register for reading.
 ;		-	Holds the address of the receiver register of the MIDI.
 ; a2	-	Holds the address of the receiver register of the keyboard.
-; a3	-	Holds the address of the mouse struct.
 _IKBD_isr:				movem.l	d0-d7/a0-a6,-(sp)
 
 						; If the keyboard doesn't have anything in its buffer,
@@ -84,108 +83,13 @@ IKBD_GET_VAL:			clr.l	d0
 						; If no mouse packets have been processed before, check
 						; for a mouse header packet.
 						tst.b	mouse_packets_received
-						beq		IKBD_CHK_MOUSE_HEADER
-						addq.b	#1,mouse_packets_received
-						cmpi.b	#2,mouse_packets_received
-						beq		IKBD_SECOND_M_PKT
-						cmpi.b	#3,mouse_packets_received
-						beq		IKBD_THIRD_M_PKT
-						; Shouldn't get here.
-						illegal
-
-
-						; Check if we have received a mouse header packet
-						; and handle it. A mouse header packet contains data
-						; on what buttons (if any) have been pressed.
-IKBD_CHK_MOUSE_HEADER:	cmpi.b	#IKBD_MIN_MOUSE_PKT_VAL,d0
+						bhi		IKBD_MANAGE_MOUSE
+						cmpi.b	#IKBD_MIN_MOUSE_PKT_VAL,d0
 						blo		IKBD_HANDLE_KEYS
-						addq.b	#1,mouse_packets_received
-						lea		_mouse,a3
-
-						btst.l	#IKBD_LCLICK_BIT,d0
-						beq		IKBD_N_LCLICK
-						move.w	#TRUE,MOUSE_LEFT_CLICK(a3)
-
-IKBD_N_LCLICK:			btst.l	#IKBD_RCLICK_BIT,d0
-						beq		IKBD_RETURN
-						move.w	#TRUE,MOUSE_RIGHT_CLICK(a3)
+IKBD_MANAGE_MOUSE:		move.b	d0,-(sp)
+						jsr		handle_mouse
+						addq.l	#2,sp
 						bra		IKBD_RETURN
-
-
-						; Handle the second mouse packet (which specifies the
-						; signed change in x). It returns early if delta x is
-						; zero or if the current x position is already at its
-						; maximum or minimum.
-IKBD_SECOND_M_PKT:		lea		_mouse,a3
-						tst.b	d0
-						beq		IKBD_RETURN
-						bmi		IKBD_X_DELTA_NEG
-						
-						cmpi.w	#SCRN_MAX_X,MOUSE_X(a3)
-						bhs		IKBD_RETURN
-						bra		IKBD_X_DELTA_ADD
-
-IKBD_X_DELTA_NEG:		tst.w	MOUSE_X(a3)
-						beq		IKBD_RETURN
-						bra		IKBD_X_DELTA_ADD
-
-						; Confirm that the new x coordinate is in range.
-IKBD_X_DELTA_ADD:		ext.w	d0
-						add.w	MOUSE_X(a3),d0
-						bmi		IKBD_X_TOO_SMALL
-						cmpi.w	#SCRN_MAX_X,d0
-						bhi		IKBD_X_TOO_LARGE
-
-						move.w	d0,MOUSE_X(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
-IKBD_X_TOO_SMALL:		clr.w	MOUSE_X(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
-IKBD_X_TOO_LARGE:		move.w	#SCRN_MAX_X,MOUSE_X(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
-
-						; Handle the third mouse packet (which specifies the
-						; signed change in y). It returns early if delta y is
-						; zero or if the current y position is already at its
-						; maximum or minimum.
-IKBD_THIRD_M_PKT:		clr.b	mouse_packets_received
-						lea		_mouse,a3
-						tst.b	d0
-						beq		IKBD_RETURN
-						bmi		IKBD_Y_DELTA_NEG
-						
-						cmpi.w	#SCRN_MAX_Y,MOUSE_Y(a3)
-						bhs		IKBD_RETURN
-						bra		IKBD_Y_DELTA_ADD
-
-IKBD_Y_DELTA_NEG:		tst.w	MOUSE_Y(a3)
-						beq		IKBD_RETURN
-						bra		IKBD_Y_DELTA_ADD
-
-						; Confirm that the new y coordinate is in range.
-IKBD_Y_DELTA_ADD:		ext.w	d0
-						add.w	MOUSE_Y(a3),d0
-						bmi		IKBD_Y_TOO_SMALL
-						cmpi.w	#SCRN_MAX_Y,d0
-						bhi		IKBD_Y_TOO_LARGE
-
-						move.w	d0,MOUSE_Y(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
-IKBD_Y_TOO_SMALL:		clr.w	MOUSE_Y(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
-IKBD_Y_TOO_LARGE:		move.w	#SCRN_MAX_Y,MOUSE_Y(a3)
-						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
-						bra		IKBD_RETURN
-
 
 						; Check if it is a make or break code.
 IKBD_HANDLE_KEYS:		bclr.l	#IKBD_BREAK_BIT,d0
@@ -271,5 +175,126 @@ IKBD_RETURN:			movem.l	(sp)+,d0-d7/a0-a6
 						bne		_IKBD_isr
 						bclr.b	#IKBD_MFP_SERVICE_BIT,MFP_IN_SERVICE_B_REG
 						rte
+
+; void handle_mouse(UINT8 packet)
+;
+; Brief: Handles mouse packets.
+;
+; Details: Every time the function is run, mouse_packets_received is incremented
+;		   until all three mouse packets are handled. 
+;
+; Register Table:
+; ---------------
+; d0	-	Holds the value from a mouse packet.
+; a3	-	Holds the address of the mouse struct.
+; a6	-	Holds the address to the start of the stack frame.
+
+MOUSE_PACKET_PARAM:		equ		8
+
+handle_mouse:			link	a6,#0
+						movem.l	d0/a3,-(sp)
+						move.b	MOUSE_PACKET_PARAM(a6),d0
+
+						addq.b	#1,mouse_packets_received
+						cmpi.b	#1,mouse_packets_received
+						beq		MOUSE_FIRST_PKT
+						cmpi.b	#2,mouse_packets_received
+						beq		MOUSE_SECOND_PKT
+						cmpi.b	#3,mouse_packets_received
+						beq		MOUSE_THIRD_PKT
+						; Shouldn't get here.
+						bra		MOUSE_RETURN
+
+
+						; A mouse header packet contains data on what buttons
+						; (if any) have been pressed.
+MOUSE_FIRST_PKT:		lea		_mouse,a3
+
+						btst.l	#MOUSE_LCLICK_BIT,d0
+						beq		MOUSE_N_LCLICK
+						move.w	#TRUE,MOUSE_LEFT_CLICK(a3)
+
+MOUSE_N_LCLICK:			btst.l	#MOUSE_RCLICK_BIT,d0
+						beq		MOUSE_RETURN
+						move.w	#TRUE,MOUSE_RIGHT_CLICK(a3)
+						bra		MOUSE_RETURN
+
+
+; Handle the second mouse packet (which specifies the
+; signed change in x). It returns early if delta x is
+; zero or if the current x position is already at its
+; maximum or minimum.
+MOUSE_SECOND_PKT:		lea		_mouse,a3
+						tst.b	d0
+						beq		MOUSE_RETURN
+						bmi		MOUSE_X_DELTA_NEG
+
+						cmpi.w	#SCRN_MAX_X,MOUSE_X(a3)
+						bhs		MOUSE_RETURN
+						bra		MOUSE_X_DELTA_ADD
+
+MOUSE_X_DELTA_NEG:		tst.w	MOUSE_X(a3)
+						beq		MOUSE_RETURN
+						bra		MOUSE_X_DELTA_ADD
+
+; Confirm that the new x coordinate is in range.
+MOUSE_X_DELTA_ADD:		ext.w	d0
+						add.w	MOUSE_X(a3),d0
+						bmi		MOUSE_X_TOO_SMALL
+						cmpi.w	#SCRN_MAX_X,d0
+						bhi		MOUSE_X_TOO_LARGE
+
+						move.w	d0,MOUSE_X(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+						bra		MOUSE_RETURN
+
+MOUSE_X_TOO_SMALL:		clr.w	MOUSE_X(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+						bra		MOUSE_RETURN
+
+MOUSE_X_TOO_LARGE:		move.w	#SCRN_MAX_X,MOUSE_X(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+						bra		MOUSE_RETURN
+
+
+; Handle the third mouse packet (which specifies the
+; signed change in y). It returns early if delta y is
+; zero or if the current y position is already at its
+; maximum or minimum.
+MOUSE_THIRD_PKT:		clr.b	mouse_packets_received
+						lea		_mouse,a3
+						tst.b	d0
+						beq		MOUSE_RETURN
+						bmi		MOUSE_Y_DELTA_NEG
+
+						cmpi.w	#SCRN_MAX_Y,MOUSE_Y(a3)
+						bhs		MOUSE_RETURN
+						bra		MOUSE_Y_DELTA_ADD
+
+MOUSE_Y_DELTA_NEG:		tst.w	MOUSE_Y(a3)
+						beq		MOUSE_RETURN
+						bra		MOUSE_Y_DELTA_ADD
+
+; Confirm that the new y coordinate is in range.
+MOUSE_Y_DELTA_ADD:		ext.w	d0
+						add.w	MOUSE_Y(a3),d0
+						bmi		MOUSE_Y_TOO_SMALL
+						cmpi.w	#SCRN_MAX_Y,d0
+						bhi		MOUSE_Y_TOO_LARGE
+
+						move.w	d0,MOUSE_Y(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+						bra		MOUSE_RETURN
+
+MOUSE_Y_TOO_SMALL:		clr.w	MOUSE_Y(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+						bra		MOUSE_RETURN
+
+MOUSE_Y_TOO_LARGE:		move.w	#SCRN_MAX_Y,MOUSE_Y(a3)
+						move.w	#TRUE,MOUSE_POS_CHANGE(a3)
+
+MOUSE_RETURN:			movem.l	(sp)+,d0/a3
+						unlk	a6
+						rts
 
 mouse_packets_received:	dc.b	0
