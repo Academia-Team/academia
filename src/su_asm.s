3962bec9897f12643269ba219adcd4978e529271
Richard Johnston <techpowerawaits@outlook.com>
2023-08-24 18:55:07 -0600

    Reimplement isSu() in assembly
    Not only is it simpler to implement, but then no complex C-assembly interactions have to be dealt with when utilizing the function in asm code.
    
    Also, the return type of Su() was changed to be signed since it does return -1 sometimes.

--- src/su_asm.s
+++ src/su_asm.s
@@ -2,7 +2,8 @@
 ; 
 ; Copyright Academia Team 2023
 
-			xdef			_Su
+				xdef			_Su
+				xdef			_isSu
 
 ; UINT32 Su(UINT32 ssp);
 ;
@@ -19,16 +20,39 @@
 ; d0	-	Holds the old system stack pointer so it can be returned.
 ; a6	-	Holds the address of the start of the stack frame.
 
-SU_SSP:		equ				8
+SU_SSP:			equ				8
 
-_Su:		link			a6,#0
-			movem.l			d1-d2/a0-a2,-(sp)
+_Su:			link			a6,#0
+				movem.l			d1-d2/a0-a2,-(sp)
 
-			move.l			SU_SSP(a6),-(sp)
-			move.w			#$20,-(sp)
-			trap			#1
-			addq.l			#6,sp
+				move.l			SU_SSP(a6),-(sp)
+				move.w			#$20,-(sp)
+				trap			#1
+				addq.l			#6,sp
 
-			movem.l			(sp)+,d1-d2/a0-a2
-			unlk			a6
-			rts
+				movem.l			(sp)+,d1-d2/a0-a2
+				unlk			a6
+				rts
+
+
+; UINT8 isSu(void);
+
+; Brief: Checks if code is in Super mode or not.
+;
+; Register Table:
+; ---------------
+; d0	-	Holds the returned value of Su() indicating whether we are in
+;			Super Mode or not.
+;		-	The return value indicating if we are in Super mode or not.
+
+_isSu:			move.l			#1,-(sp)
+				bsr				_Su
+				addq.l			#4,sp
+				tst.l			d0
+				bmi				ISU_IS_SUPER
+				clr.b			d0
+				bra				ISU_RETURN
+
+ISU_IS_SUPER:	move.b			#1,d0
+
+ISU_RETURN:		rts
